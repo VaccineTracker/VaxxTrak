8c5ee03de415d0e81df7ccdff06080af6e3f8715
mggower <gowermikey@gmail.com>
2021-01-13 12:42:53 -0500

    update example

--- api/api.js
+++ api/api.js
@@ -1,4 +1,4 @@
-import hash from './states.js';
+import hash from './states';
 
 const apiKey =
   'CpgQ68eNMq9Z3twQDOuNWGfQGSjzRIxDcdrHP45P6WXJg7OzVBz8F62H9XYLmYYP';
@@ -38,3 +38,17 @@ async function fromZipcode(zipcode) {
 }
 
 export { request, fromZipcode };
+
+/**
+ * EXAMPLE: 
+import { request, fromZipcode } from './api.js';
+
+const state = await fromZipcode('11221');
+
+const moderna = await request('moderna', state);
+console.log(moderna);
+
+const pfizer = await request('pfizer', state);
+console.log(pfizer);
+
+ */
