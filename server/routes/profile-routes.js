490d7ed6ce81379dd0d6449e5578c9212fee77b3
mggower <gowermikey@gmail.com>
2021-01-12 19:02:23 -0500

    merging from main

--- server/routes/profile-routes.js
+++ server/routes/profile-routes.js
@@ -1,20 +1,18 @@
 const router = require('express').Router();
 
 const authCheck = (req, res, next) => {
-    if(!req.user) {
-        // if user is not logged in
-        res.redirect('/auth/login');
-    }
-    else {
-        // if logged in
-        next();
-    }
-}
+  if (!req.user) {
+    // if user is not logged in
+    res.redirect('/auth/login');
+  } else {
+    // if logged in
+    next();
+  }
+};
 
 router.get('/', authCheck, (req, res) => {
-    // render profile page
-    res.render('profile', {user: req.user});
+  // render profile page
+  res.render('profile', { user: req.user });
 });
-
 
 module.exports = router;
